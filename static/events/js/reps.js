feacfcbb5a10b62e04c84c9ecfa232b76b844bb6
Giovanny Andres Gongora Granada <gioyik@gmail.com>
2015-07-05 00:51:33 -0500

    date parsing

--- static/events/js/reps.js
+++ static/events/js/reps.js
@@ -1,5 +1,10 @@
 $( document ).ready(function() {
  
+    getMonth = function (v) {
+        var n = ["", "Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"];
+        return n[v];
+    };
+
     var dateToday = new Date(),
         dateString = dateToday.getFullYear() + "-" + (dateToday.getMonth()+1) + "-" + dateToday.getDate(),
         urlReps = "https://reps.mozilla.org/api/v1/event/?offset=0&limit=0&start__gte=" + dateString + "&query=Colombia";
@@ -19,6 +24,9 @@ $( document ).ready(function() {
                     <p class='event-info l-ocation'></p> \
                 </div> \
               </div>"
+              
+    var yearRegexp = /\d{4}\-\d{1,2}\-\d{1,2}/;
+    
     $.ajax({
       method: "POST",
       type: "POST",
@@ -27,12 +35,15 @@ $( document ).ready(function() {
       url: urlReps
     }).done(function( data ) {
           $.each( data.objects, function( i, item ) {
+            var dateSplit = yearRegexp.exec(item.local_start);
+            var convert = String(dateSplit);
+            var finalDate = convert.split("-");
             $('body').append(html);
-            $('.date-large').attr('href', item.event_url)
-            $('.event-detail a').attr('href', item.event_url)
-            $('.res_icon .calendar-month').text(item.local_start);
-            $('.res_icon .calendar-day').text(item.local_start);
-            $('.res_icon .calendar-year').text(item.local_start);
+            $('.date-large').attr('href', item.event_url);
+            $('.event-detail a').attr('href', item.event_url);
+            $('.res_icon .calendar-month').text(getMonth(finalDate[1]));
+            $('.res_icon .calendar-day').text(finalDate[2]);
+            $('.res_icon .calendar-year').text(finalDate[0]);
             $('.event-detail h2').text(item.name);
             $('.event-info').text(item.description);
           });
